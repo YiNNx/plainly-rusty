817570c1a038a0bd64840281a12adc7d82e2e5e1
yinn_x <yinnnxt@gmail.com>
2023-11-17 21:21:58 +0800

    feat: update sql for data schema

--- scripts/plainly_rusty_create.sql
+++ scripts/plainly_rusty_create.sql
@@ -1,67 +1,99 @@
--- Create the Tags table
-CREATE TABLE Tags (
-    tid SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+CREATE TYPE user_role AS ENUM ('USER', 'OWNER');
+
+CREATE TYPE post_status AS ENUM ('PRIVATE', 'PUBLIC', 'DELETED');
+
+CREATE TYPE comment_status AS ENUM ('PUBLIC', 'DELETED');
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    github_id INT UNIQUE NOT NULL,
+    role user_role DEFAULT 'USER' :: user_role
 );
 
--- Create the Posts table
-CREATE TABLE Posts (
-    pid SERIAL PRIMARY KEY,
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     content TEXT NOT NULL,
     views INT DEFAULT 0,
     summary TEXT,
-    status VARCHAR(7) DEFAULT 'PUBLIC' CHECK (status IN ('PRIVATE', 'PUBLIC', 'DELETED'))
+    status post_status DEFAULT 'PUBLIC' :: post_status,
+    CONSTRAINT unique_title_content UNIQUE (title, content)
 );
 
--- Create the PostTags table
-CREATE TABLE PostTags (
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE post_tags (
     post_id INT,
     tag_id INT,
     PRIMARY KEY (post_id, tag_id),
-    FOREIGN KEY (post_id) REFERENCES Posts(pid),
-    FOREIGN KEY (tag_id) REFERENCES Tags(tid)
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id),
+    CONSTRAINT unique_post_tag UNIQUE (post_id, tag_id)
 );
 
--- Create the Users table
-CREATE TABLE Users (
-    uid SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    role VARCHAR(5) DEFAULT 'USER' CHECK (role IN ('USER', 'OWNER'))
-);
-
--- Create the Comments table
-CREATE TABLE Comments (
-    cid SERIAL PRIMARY KEY,
-    parent_cid INT,
-    post_id INT,  
-    username VARCHAR(255),
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    parent_id INT,
+    post_id INT,
+    github_id INT,
     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     content TEXT NOT NULL,
-    status VARCHAR(7) DEFAULT 'PUBLIC' CHECK (status IN ('PRIVATE', 'PUBLIC', 'DELETED')),
-    FOREIGN KEY (parent_cid) REFERENCES Comments(cid),
-    FOREIGN KEY (post_id) REFERENCES Posts(pid), 
-    FOREIGN KEY (username) REFERENCES Users(username)
+    status comment_status DEFAULT 'PUBLIC' :: comment_status,
+    FOREIGN KEY (parent_id) REFERENCES comments(id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (github_id) REFERENCES users(github_id),
+    CONSTRAINT unique_user_comment UNIQUE (post_id, github_id, content)
 );
 
-
 ---
+INSERT INTO
+    users (github_id, role)
+VALUES
+    (86813521, 'USER'),
+    (86649490, 'OWNER');
 
-INSERT INTO Users (username, role) VALUES ('YiNNx', 'OWNER');
+INSERT INTO
+    tags (name)
+VALUES
+    ('Technology'),
+    ('Programming'),
+    ('Database');
 
-INSERT INTO Tags (name) VALUES ('Tag1');
-INSERT INTO Tags (name) VALUES ('Tag2');
+INSERT INTO
+    posts (title, content, summary)
+VALUES
+    (
+        'Getting Started with PostgreSQL',
+        'PostgreSQL is a powerful open-source relational database...',
+        'Learn the basics of PostgreSQL.'
+    ),
+    (
+        'Web Development Best Practices',
+        'Follow these best practices for building scalable and maintainable web applications...',
+        'Improve your web development skills.'
+    );
 
-INSERT INTO Posts (title, content, summary, status) VALUES ('Post1 Title', 'Post1 Content', 'Post1 Summary', 'PUBLIC');
-INSERT INTO Posts (title, content, summary, status) VALUES ('Post2 Title', 'Post2 Content', 'Post2 Summary', 'PRIVATE');
+INSERT INTO
+    post_tags (post_id, tag_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 2),
+    (2, 3);
 
-INSERT INTO PostTags (post_id, tag_id) VALUES (1, 1);
-INSERT INTO PostTags (post_id, tag_id) VALUES (1, 2);
-INSERT INTO PostTags (post_id, tag_id) VALUES (2, 2);
+INSERT INTO
+    comments (post_id, github_id, content)
+VALUES
+    (1, 86813521, 'Great introduction!'),
+    (1, 86649490, 'Looking forward to more posts.'),
+    (2, 86813521, 'These best practices are helpful!');
 
-INSERT INTO Users (username, role) VALUES ('User1', 'USER');
-INSERT INTO Users (username, role) VALUES ('User2', 'USER');
-
-INSERT INTO Comments (parent_cid,post_id, username, content, status) VALUES (NULL,1,  'User1','Comment1 Content', 'PUBLIC');
-INSERT INTO Comments (parent_cid,post_id, username, content, status) VALUES (1,1, 'User2',1, 'Comment2 Content', 'PRIVATE');
+INSERT INTO
+    comments (parent_id, post_id, github_id, content)
+VALUES
+    (1, 1, 86649490, 'Thanks!'),
+    (4, 2, 86813521, 'Glad you found them helpful!');
